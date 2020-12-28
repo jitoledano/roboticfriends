9e98307bb2979ff10c286a860ff751719b44cf03
jitoledano <jitoledano98@gmail.com>
2020-12-27 20:17:22 -0700

    end test

--- src/Components/cardList.js
+++ src/Components/cardList.js
@@ -2,10 +2,7 @@ import React from 'react';
 import Card from'./Card';
 
 const CardList = ({robots}) =>{
-    if(true){
-        throw new Error('NOOOO')
-    }
-    const cardArray = robots.map((user, i) =>{
+        const cardArray = robots.map((user, i) =>{
         return( <Card key={"name"+ i}id={robots[i].id} name={robots[i].name} email={robots[i].email}/>)
     })
     return(
